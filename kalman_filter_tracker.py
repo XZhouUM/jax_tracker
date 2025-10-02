5ea2d0251c2fc0750c5e7203e4e27dbc5ab0a038
XZhouUM <zhouxin.way@gmail.com>
2025-10-02 00:21:37 -0400

    Finish up the Kalman filter for a single track.

--- kalman_filter_tracker.py
+++ kalman_filter_tracker.py
@@ -1,5 +1,5 @@
 import jax.numpy as jnp
-from typing import NamedTuple, Dict, Any, Type
+from typing import NamedTuple, Dict, Type, Tuple, Any
 from data_association.data_association import DataAssociation
 from motion_models.motion_model import MotionModel
 from measurement_models.measurement_model import MeasurementModel
@@ -21,10 +21,9 @@ class KalmanFilterTrackerState(NamedTuple):
 class KalmanFilterTracker:
     def __init__(self,
                  initial_state: KalmanFilterTrackerState,
-                 data_association_class: Type(DataAssociation),
-                 data_association_params: Dict{str, Any},
-                 motion_model_class: Type(MotionModel),
-                 measurement_model_class: Type(MeasurementModel)) -> None:
+                 data_association_class: Type[DataAssociation],
+                 data_association_params: Dict[str, Any],
+                 motion_model_class: Type[MotionModel]) -> None:
         """
         Initialize the KalmanFilterTracker.
 
@@ -33,11 +32,9 @@ class KalmanFilterTracker:
             data_association_class: The data association class to use.
             data_association_params: The parameters for the data association class.
             motion_model_class: The motion model class to use.
-            measurement_model_class: The measurement model class to use.
         """
         self.data_association = data_association_class(**data_association_params)
         self.motion_model = motion_model_class()
-        self.measurement_model = measurement_model_class()
     
         self.state = initial_state
 
@@ -51,22 +48,57 @@ class KalmanFilterTracker:
                     associated_data = jnp.concatenate((associated_data, [measurement]))
         return associated_data
    
-    def _time_update(self, dt: float) -> None:
+    def _time_update(self, dt: float, Q: jnp.ndarray) -> None:
         """
         Predict the state at the next time step.
-        """
-        self.state.x = self.motion_model.predict(dt) 
 
-    def _measurement_update(self, state: jnp.ndarray, measurements: dict) -> None:
+        Args:
+            dt: The time step.
+            Q: The process noise covariance matrix.
+        """
+        self.state.x = self.motion_model.predict(dt)
+        self.state.P = self.motion_model.jacobian() @ self.state.P @ self.motion_model.jacobian().T + Q
+
+    def _measurement_update(self,
+                            measurements: Dict[jnp.ndarray, Tuple[Type[MeasurementModel], jnp.ndarray]]) -> None:
         """
         Update the state based on the measurements.
+
+        Args:
+            measurements: A dictionary between the measurement, and its measurement
+                model and measurement noise covariance matrix.
         """
         # associate the measurements to the track.
-        associated_data = self._associate(self, state, measurements)
+        associated_data = self._associate(self, self.state.x, measurements)
 
         if associated_data is not None:
-            self.state.x = self.measurement_model.update(state, associated_data)
+            for measurement, (measurement_model, R) in associated_data:
+                # Predicted measurement and Jacobian
+                measurement_prediction = measurement_model.predict_measurement(self.state.x)
+                H = measurement_model.jacobian(self.state.x)
 
-    def update(self, state: jnp.ndarray, dt: float, measurements: dict) -> None:
+                # Innovation / residual
+                y = measurement - measurement_prediction
+
+                # Innovation covariance
+                S = H @ self.state.P @ H.T + R
+
+                # Kalman gain
+                K = self.state.P @ H.T @ jnp.linalg.inv(S)
+
+                # Update state and covariance
+                self.state.x = self.state.x + K @ y
+                self.state.P = (jnp.eye(self.state.P.shape[0]) - K @ H) @ self.state.P
+
+
+    def update(self, dt: float, measurements: dict) -> None:
+        """
+        Update the state of the tracker.
+
+        Args:
+            dt: The time step.
+            measurements: A dictionary between the measurement, and its measurement
+                model and measurement noise covariance matrix.
+        """
         self._time_update(self, dt)
-        self._measurement_update(self, state, measurements)
+        self._measurement_update(self, measurements)
