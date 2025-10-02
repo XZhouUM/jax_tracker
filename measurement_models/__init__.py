e1f21744126ea439508624f6c363b22eeb5c3eca
XZhouUM <zhouxin.way@gmail.com>
2025-10-02 19:43:35 -0400

    Make packages out of modules for easier management down the road.

---
+++ measurement_models/__init__.py
@@ -0,0 +1,1 @@
+# Measurement models package
--- measurement_models/radar_measurement_model.py
+++ measurement_models/radar_measurement_model.py
@@ -1,4 +1,4 @@
-from measurement_model import MeasurementModel
+from measurement_models.measurement_model import MeasurementModel
 import jax.numpy as jnp
 
 
@@ -8,6 +8,7 @@ class RadarMeasurement(MeasurementModel):
 
     state = [x, y, vx, vy], measurement = [range, range_rate, azimuth]
     """
+
     def __init__(self):
         super().__init__()
 
@@ -28,21 +29,21 @@ class RadarMeasurement(MeasurementModel):
         # Range and range squared.
         r_sq = x**2 + y**2
         r = jnp.sqrt(x**2 + y**2)
-        
+
         H = jnp.zeros((3, 4))
-        
+
         # d_range/d_state
         H = H.at[0, 0].set(x / r)
         H = H.at[0, 1].set(y / r)
-        
+
         # d_range_rate/d_state
-        H = H.at[1, 0].set(vx / r - x * (x*vx + y*vy) / r**3)
-        H = H.at[1, 1].set(vy / r - y * (x*vx + y*vy) / r**3)
+        H = H.at[1, 0].set(vx / r - x * (x * vx + y * vy) / r**3)
+        H = H.at[1, 1].set(vy / r - y * (x * vx + y * vy) / r**3)
         H = H.at[1, 2].set(x / r)
         H = H.at[1, 3].set(y / r)
-        
+
         # d_azimuth/d_state
         H = H.at[2, 0].set(-y / r_sq)
         H = H.at[2, 1].set(x / r_sq)
-        
+
         return H
